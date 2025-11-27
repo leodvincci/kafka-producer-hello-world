adc9e1bb3989da0884aad552f3f86e5826a2161b
leodvincci <lpenrose@depaul.edu>
2025-11-27 01:45:55 -0600

    added json serializer config

--- src/main/java/com/penrose/springkafkaproducerhelloworld/HelloProducerService.java
+++ src/main/java/com/penrose/springkafkaproducerhelloworld/HelloProducerService.java
@@ -6,23 +6,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class HelloProducerService {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, Greeting> kafkaTemplate;
 
-    public HelloProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+    public HelloProducerService(KafkaTemplate<String, Greeting> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void sendHello() {
-        kafkaTemplate.send("hello-world", "Hello, Kafka!");
+//    public void sendHello() {
+//        kafkaTemplate.send("hello-world", "Hello, Kafka!");
+//    }
+
+    public void sendGreeting() {
+        Greeting greeting = new Greeting("Penrose", "Hello, JSON!");
+        kafkaTemplate.send("goodnite-world",greeting);
     }
 
-
-
-    public void sendGoodNight() {
-        kafkaTemplate.send("goodnite-world", "Goodnight, Kafka!");
-    }
-
-    public void sendCustom(String message,String key) {
-        kafkaTemplate.send("goodnite-world", key, message);
-    }
+//
+//    public void sendGoodNight() {
+//        kafkaTemplate.send("goodnite-world", "Goodnight, Kafka!");
+//    }
+//
+//    public void sendCustom(String message,String key) {
+//        kafkaTemplate.send("goodnite-world", key, message);
+//    }
 }
